eecff3847c9f899f7af1a9cae558fc4eaeebb36b
Michael Sarahan <msarahan@gmail.com>
2017-11-30 15:23:09 -0600

    cb3-ize

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,19 @@
 #!/bin/sh
 
-export CC=${PREFIX}/bin/h5cc
-export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
-export CFLAGS="-fPIC"
+# export CC=${PREFIX}/bin/h5cc
+# export CXX=${PREFIX}/bin/h5c++
+# export DYLD_FALLBACK_LIBRARY_PATH=${PREFIX}/lib
+# export CFLAGS="-fPIC"
 
-export HDF5_LDFLAGS="-L ${PREFIX}/lib"
+# export HDF5_LDFLAGS="-L ${PREFIX}/lib"
 
-./configure --prefix=${PREFIX} --with-hdf5=${PREFIX} --with-zlib=${PREFIX}
+./configure --prefix=${PREFIX} \
+            --build=${BUILD} \
+            --host=${HOST} \
+            --with-hdf5=${PREFIX} \
+            --with-zlib=${PREFIX} \
+            --enable-shared=yes \
+            --enable-static=no
 
 make
 make check
